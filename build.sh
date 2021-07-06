f53a1a0bbd3516a98da559f0c963932af6be0e95
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-07-06 13:48:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ ARCH=$(uname -m)
 LD_DEBUG=libs docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
 docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-docker push quay.io/rpsene/multi-arch-travis:$ARCH
+docker push quay.io/rohan_shinde_98/multi-arch-travis:$ARCH
